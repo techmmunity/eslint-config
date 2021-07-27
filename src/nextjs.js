876ebd1813b87f6622ad63b24f30eb79315c275f
Henrique Leite <henriqueleite616@gmail.com>
2021-07-27 17:57:25 -0600

    Update NextJs Module (#64)

--- src/nextjs.js
+++ src/nextjs.js
@@ -1,4 +1,5 @@
 module.exports = {
+	plugins: ["@next/eslint-plugin-next"],
 	overrides: [
 		{
 			files: ["pages/**/*.{js,jsx,ts,tsx}", "src/pages/**/*.{js,jsx,ts,tsx}"],
@@ -39,7 +40,26 @@ module.exports = {
 		"filenames/match-regex": ["error", "^_?[a-zA-Z0-9-.@]+$", true],
 		/**
 		 * React
+		 *
+		 * https://github.com/yannickcr/eslint-plugin-react#list-of-supported-rules
 		 */
 		"react/react-in-jsx-scope": "off",
+		/**
+		 * Plugins - NextJs
+		 *
+		 * https://nextjs.org/docs/basic-features/eslint#eslint-plugin
+		 */
+		"next/google-font-display": "error",
+		"next/google-font-preconnect": "error",
+		"next/link-passhref": "error",
+		"next/no-css-tags": "error",
+		"next/no-document-import-in-page": "error",
+		"next/no-head-import-in-document": "error",
+		"next/no-html-link-for-pages": "error",
+		"next/no-img-element": "error",
+		"next/no-page-custom-font": "error",
+		"next/no-sync-scripts": "error",
+		"next/no-title-in-document-head": "error",
+		"next/no-unwanted-polyfillio": "error",
 	},
 };
